d6be40be42fb3b590b8a28e727ed0a33b1df177a
Diana Darienko <diana.darienko21@gmail.com>
2025-08-28 16:23:17 +0300

    update img src

--- src/types/Wine.tsx
+++ src/types/Wine.tsx
@@ -14,16 +14,16 @@ export enum WineValue {
 }
 
 export enum WineImg {
-  BILLY_BUTTON_COL_FONDO = 'public/wines_images/Billy Button Col Fondo.png',
-  COBAW_RIDGE_PYRENEES_ROSÉ = 'public/wines_images/Cobaw Ridge Pyrenees Rosé.png',
-  DOMAINE_GAYDA_SPHÈRE = 'public/wines_images/Domaine Gayda Sphère.png',
-  FUNKSTILLE_SKIN_CONTACT = 'public/wines_images/Funkstille Skin Contact.png',
-  JAUMA_ORIGINS_SKIN_CONTACT = 'public/wines_images/Jauma Origins Skin Contact.png',
-  LANSDOWNE_SUPER_PLONK = 'public/wines_images/Lansdowne Super Plonk.png',
-  OLD_MATES_PINOT_NOIR = 'public/wines_images/Old Mates Pinot Noir 2024.png',
-  OLD_MATES_ROSÉ = 'public/wines_images/Old Mates Rosé 2024.png',
-  SÈT_E_MÈZ_PÉT_NAT_ROSÉ = 'public/wines_images/Sèt e Mèz Pét Nat Rosé.png',
-  VINO_FRIENDO_ROSATO = 'public/wines_images/Vino Friendo Rosato 2024.png',
+  BILLY_BUTTON_COL_FONDO = '/wines_images/Billy Button Col Fondo.png',
+  COBAW_RIDGE_PYRENEES_ROSÉ = '/wines_images/Cobaw Ridge Pyrenees Rosé.png',
+  DOMAINE_GAYDA_SPHÈRE = '/wines_images/Domaine Gayda Sphère.png',
+  FUNKSTILLE_SKIN_CONTACT = '/wines_images/Funkstille Skin Contact.png',
+  JAUMA_ORIGINS_SKIN_CONTACT = '/wines_images/Jauma Origins Skin Contact.png',
+  LANSDOWNE_SUPER_PLONK = '/wines_images/Lansdowne Super Plonk.png',
+  OLD_MATES_PINOT_NOIR = '/wines_images/Old Mates Pinot Noir 2024.png',
+  OLD_MATES_ROSÉ = '/wines_images/Old Mates Rosé 2024.png',
+  SÈT_E_MÈZ_PÉT_NAT_ROSÉ = '/wines_images/Sèt e Mèz Pét Nat Rosé.png',
+  VINO_FRIENDO_ROSATO = '/wines_images/Vino Friendo Rosato 2024.png',
 }
 
 export interface Wine {
