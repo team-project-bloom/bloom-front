4f5c8683e77d8c94cd71db40027f33b4caeb226e
Diana Darienko <diana.darienko21@gmail.com>
2025-08-28 16:43:40 +0300

    change ga

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize("G-3S2D0D7EX8");
+  ReactGA.initialize("G-MBSLR0FCG6");
 };
 
 export const logPageView = (path: string) => {
